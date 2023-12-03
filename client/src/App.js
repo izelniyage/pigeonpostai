da31556d07a39147f29cf59d47f3c2e54aa6969a
izelniyage <izel84@gmail.com>
2023-12-02 23:35:09 -0500

    react components

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+      <div className="App">
+        <header className="App-header">
+          <h1>PigeonPostAI</h1>
+          <p>Welcome to PigeonPostAI - Your AI-powered email summarization tool.</p>
+          <p>Coming Soon...</p>
+        </header>
+      </div>
+  );
+}
+
+export default App;
